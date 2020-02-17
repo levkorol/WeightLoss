e91eec479f820fe8077fa5b8e756c00f01e226bd
kuelye <kuelye@gmail.com>
2020-02-17 17:28:09 +0300

    17.02

--- app/src/main/java/com/levkorol/weightloss/ui/RegistrationActivity.kt
+++ app/src/main/java/com/levkorol/weightloss/ui/RegistrationActivity.kt
@@ -1,7 +1,8 @@
-package com.levkorol.weightloss
+package com.levkorol.weightloss.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.levkorol.weightloss.R
 
 class RegistrationActivity : AppCompatActivity() {
 
