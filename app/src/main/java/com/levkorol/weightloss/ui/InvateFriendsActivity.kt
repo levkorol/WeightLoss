aebedc41a1d490b5c2117553d55f750b87695639
lev <sasiskajik@mail.ru>
2020-03-12 12:18:27 +0300

    komment

---
+++ app/src/main/java/com/levkorol/weightloss/ui/InvateFriendsActivity.kt
@@ -0,0 +1,12 @@
+package com.levkorol.weightloss.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InvateFriendsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invate_friends)
+    }
+}
---
+++ app/src/main/java/com/levkorol/weightloss/ui/PremiumActivity.kt
@@ -0,0 +1,12 @@
+package com.levkorol.weightloss.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PremiumActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_premium)
+    }
+}
